adf8f47d9145a53e796e3a179a5573a8993b96be
Penpitcha Piyawaranont <135194778+incluDna@users.noreply.github.com>
2026-02-22 15:21:45 +0700

    Create App.tsx

---
+++ Cafe/src/App.tsx
@@ -0,0 +1,11 @@
+import Timer from "./components/Timer";
+import PlaylistSection from "./components/PlaylistSection";
+
+export default function App() {
+  return (
+    <div>
+      <Timer />
+      <PlaylistSection />
+    </div>
+  );
+}
